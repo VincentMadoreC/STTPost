d950052a8d641f2e8276369abcdf6c5813334313
Vincent Madore Charron <vmado051@uottawa.ca>
2019-03-25 16:21:02 -0400

    leaving for later

--- app/src/main/java/ca/canada/sttpost/Speech.java
+++ app/src/main/java/ca/canada/sttpost/Speech.java
@@ -1,4 +1,28 @@
-package ca.canada.sttpost;
-
-public class Speech {
-}
+//package ca.canada.sttpost;
+//
+//import android.content.ActivityNotFoundException;
+//import android.content.Intent;
+//import android.speech.RecognizerIntent;
+//
+//import java.util.Locale;
+//
+//import android.support.v7.app.AppCompatActivity;
+//
+//public class Speech {
+//
+//    private final int REQ_CODE_SPEECH_INPUT = 100;
+//
+//    public void askSpeechInput() {
+//        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+//        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+//                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+//        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+//        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+//                "Hi speak something");
+//        try {
+//            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+//        } catch (ActivityNotFoundException a) {
+//
+//        }
+//    }
+//}
